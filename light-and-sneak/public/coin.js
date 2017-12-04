ab1aa5030b7536e2f1c6fc1f41b79572d80a435a
Kratos <nrupsis@yahoo.com>
2017-12-03 20:40:50 -0500

    added coin and coin animation

---
+++ light-and-sneak/public/coin.js
@@ -0,0 +1,12 @@
+function Coin(game, x, y, sprite){
+    coin = game.add.sprite(x, y, sprite);
+    coin.ANIM_SPEED = 10;
+    coin.width = 25;
+    coin.height = 25;
+    game.physics.p2.enable(coin);
+    coin.body.static = true;
+    
+    coin.animations.add('walk');
+    coin.animations.play('walk', coin.ANIM_SPEED, true);
+
+}
